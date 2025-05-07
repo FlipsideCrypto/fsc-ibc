9ba7cd8fbf5135e7fc0a20b6c60a030a9926e1d8
Mike Stepanovic <mike.stepanovic@flipsidecrypto.com>
2025-05-07 12:29:18 -0600

    updated macros and makefile

--- macros/global/variables/projects/cosmos.sql
+++ macros/global/variables/projects/cosmos.sql
@@ -2,7 +2,7 @@
     {% set vars = {
         'GLOBAL_PROJECT_NAME': 'cosmos',
         'GLOBAL_NODE_PROVIDER': 'quicknode',
-        'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/cosmos/quicknode/mainnet',
+        'GLOBAL_NODE_VAULT_PATH': 'vault/prod/cosmos/quicknode/mainnet',
         'GLOBAL_NODE_URL': '{service}/{Authentication}',
         'GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS': '',
         'MAIN_SL_BLOCKS_PER_HOUR': 3600
