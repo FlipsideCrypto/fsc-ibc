9e4ccd4fcdc1ad919bdaafd8e29e06a6040d83fd
Mike Stepanovic <mike.stepanovic@flipsidecrypto.com>
2025-05-07 12:59:23 -0600

    GHA modifications

--- macros/functions/streamline/models.sql
+++ macros/functions/streamline/models.sql
@@ -1,77 +1,70 @@
 {% macro streamline_external_table_query_v2(
         model,
-        partition_function,
-        partition_name,
-        other_cols
-) %}
-WITH meta AS (
-    SELECT
-        LAST_MODIFIED::timestamp_ntz AS _inserted_timestamp,
-        file_name,
-        {{ partition_function }} AS {{ partition_name }}
-    FROM
-        TABLE(
-            information_schema.external_table_file_registration_history(
-                start_time => DATEADD('day', -3, CURRENT_TIMESTAMP()),
-                table_name => '{{ source( "bronze_streamline", model) }}')
-            ) A
-)
-SELECT
-    {{ other_cols }},
-    _inserted_timestamp,
-    s.{{ partition_name }},
-    s.value AS VALUE,
-    file_name
-FROM
-    {{ source(
-        "bronze_streamline",
-        model
-    ) }}
-    s
-JOIN 
-    meta b
-    ON b.file_name = metadata$filename
-    AND b.{{ partition_name }} = s.{{ partition_name }}
-WHERE
-    b.{{ partition_name }} = s.{{ partition_name }}
-    
+        partition_function
+    ) %}
+
+      {% set days = var("BRONZE_LOOKBACK_DAYS")%}
+
+    WITH meta AS (
+        SELECT
+            last_modified AS inserted_timestamp,
+            file_name,
+            {{ partition_function }} AS partition_key
+        FROM
+            TABLE(
+                information_schema.external_table_file_registration_history(
+                    start_time => DATEADD('day', -ABS({{days}}), CURRENT_TIMESTAMP()),
+                    table_name => '{{ source( "bronze_streamline", model) }}')
+                ) A
+            )
+        SELECT
+            s.*,
+            b.file_name,
+            inserted_timestamp
+        FROM
+            {{ source(
+                "bronze_streamline",
+                model
+            ) }}
+            s
+            JOIN meta b
+            ON b.file_name = metadata$filename
+            AND b.partition_key = s.partition_key
+        WHERE
+            b.partition_key = s.partition_key
+            AND DATA :error IS NULL
 {% endmacro %}
 
 {% macro streamline_external_table_FR_query_v2(
         model,
-        partition_function,
-        partition_name,
-        other_cols
-) %}
-WITH meta AS (
-    SELECT
-        LAST_MODIFIED::timestamp_ntz AS _inserted_timestamp,
-        file_name,
-        {{ partition_function }} AS {{ partition_name }}
-    FROM
-        TABLE(
-            information_schema.external_table_files(
-                table_name => '{{ source( "bronze_streamline", model) }}'
-            )
-        ) A
-)
+        partition_function
+    ) %}
+    WITH meta AS (
+        SELECT
+            registered_on AS inserted_timestamp,
+            file_name,
+            {{ partition_function }} AS partition_key
+        FROM
+            TABLE(
+                information_schema.external_table_files(
+                    table_name => '{{ source( "bronze_streamline", model) }}'
+                )
+            ) A
+    )
 SELECT
-    {{ other_cols }},
-    _inserted_timestamp,
-    s.{{ partition_name }},
-    s.value AS VALUE,
-    file_name
+    s.*,
+    b.file_name,
+    inserted_timestamp
 FROM
     {{ source(
         "bronze_streamline",
         model
     ) }}
     s
-JOIN 
-    meta b
+    JOIN meta b
     ON b.file_name = metadata$filename
-    AND b.{{ partition_name }} = s.{{ partition_name }}
+    AND b.partition_key = s.partition_key
 WHERE
-    b.{{ partition_name }} = s.{{ partition_name }}
-    
+    b.partition_key = s.partition_key
+    AND DATA :error IS NULL
 {% endmacro %}
